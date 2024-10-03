b4025b6e3658ec3518d03177e9baff59f28b3373
Thomas Gaubert <thomas.gaubert49@gmail.com>
2024-10-03 12:50:24 +0200

    :sparkles: 2024 Devfest adjustements

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,62 @@
 <template>
   <div class="msc-updating" :class="{ '-visible': isUpdating }">
-    <span class="msc-updating__loader"></span>
-    Mise à jour en cours...
+	  <div class="msc-updating__content">
+	    <span class="msc-updating__loader"></span>
+	    Mise à jour en cours...
+	  </div>
   </div>
   <div class="msc-last-update" :class="{ '-visible': !isUpdating }">
-    <span class="msc-last-update__label">Dernière mise à jour:</span>
-    {{ lastUpdateDate }}
+	  <div class="msc-last-update__content">
+		  <span class="msc-last-update__label">Dernière mise à jour :</span>
+		  {{ lastUpdateDate }}
+	  </div>
   </div>
-  <div class="msc-zenika-container">
-    <div class="msc-zenika-logo"></div>
-    <h1 class="msc-title">Zenika Nantes</h1>
-  </div>
+	<div class="msc-zenika-logo"></div>
   <main class="msc-page">
-    <div class="msc-header">
-      <div class="msc-header__logo"></div>
-      <span class="msc-header__divider"></span>
-      <div class="msc-header__content">
-        <h2 class="msc-title">Tableau des scores</h2>
-        <h3 class="msc-title">Project-n1870</h3>
-      </div>
-    </div>
+	  <h2 class="msc-title">Tableau des scores</h2>
     <p class="msc-subtitle" v-if="scores.length === 0">
       Aucun score à afficher !
     </p>
     <div class="msc-top-score" v-if="scores.length > 0">
       <div class="msc-top-score__card" v-for="(score, index) in scores.slice(0, 3)" :key="score[0]">
-        <p class="msc-top-score__rank"> {{ index + 1 }}</p>
-        <div class="msc-top-score__content">
-          <p class="msc-top-score__score">
-            {{ (score[2]) }}
-            <span class="msc-top-score__score__pts">pts</span>
-          </p>
-          <p class="msc-top-score__pseudo">{{ score[1] }}</p>
-          <p class="msc-top-score__date">{{ formatDate(score[0]) }}</p>
+        <div class="msc-top-score__card__header">
+	        <p class="msc-top-score__rank"> {{ index + 1 }}</p>
+	        <p class="msc-top-score__pseudo">{{ score[0] }}</p>
         </div>
+	      <div class="msc-top-score__score">
+		      <div class="msc-top-score__score__stat">
+			      <p class="msc-top-score__score__label">Score</p>
+			      <p class="msc-top-score__score__value">
+				      {{ score[3] }}
+			      </p>
+		      </div>
+		      <div class="msc-top-score__score__stat">
+			      <p class="msc-top-score__score__label">Temps</p>
+			      <p class="msc-top-score__score__value">
+				      {{ score[2] }}
+			      </p>
+		      </div>
+	      </div>
       </div>
     </div>
     <table class="msc-score-table" v-if="scores.length > 3">
       <thead>
         <tr>
-          <th>n°</th>
+          <th>Username</th>
           <th>Score</th>
-          <th>Pseudo</th>
-          <th>Date</th>
+          <th>Temps</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(score, index) in scores.slice(3, 10)" :key="score[0]">
-          <td>
-            <span class="msc-score-table__rank">{{ index + 4 }}</span>
-          </td>
-          <td class="msc-score-table__score">
-            {{ score[2] }}
-            <span class="msc-score-table__score__pts">pts</span>
-          </td>
-          <td>{{ score[1] }}</td>
-          <td class="msc-score-table__date" v-html="formatDate(score[0]).split(' ').join('<br>')">
-          </td>
+	        <td>
+		       <div class="msc-score-table__pseudo">
+			       <span class="msc-score-table__rank">{{ index + 4 }}</span>
+			       {{ score[0] }}
+		       </div>
+	        </td>
+          <td>{{ score[3] }}</td>
+	        <td> {{ score[2] }}</td>
         </tr>
       </tbody>
     </table>
@@ -69,11 +69,11 @@ const isUpdating = ref<boolean>(true);
 const lastUpdateDate = ref<string>("");
 let scores = ref<Array<Array<string>>>([]);
 
-const SPREADSHEET_ID: string = "1G9simK3R8ByIOrFMhmcDfon4GF-70_stKzjdztV5r5Q";
+const SPREADSHEET_ID: string = "1U5A97xU_V6hAa4p2_pvPZQL4552lRpqSObL5H1CB9oI";
 
 async function loadSpreadSheet(apiKey: any): Promise<void> {
   isUpdating.value = true;
-  const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/A2:D?key=${apiKey}`, {
+  const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/A2:E?key=${apiKey}`, {
     method: "GET",
   });
   const data = await response.json();
@@ -89,7 +89,7 @@ function updateScore(values: Array<Array<string>>) {
   lastUpdateDate.value = formatDate(date.toString());
   scores.value = values;
   scores.value.sort((a, b) => {
-    if (parseInt(a[2]) < parseInt(b[2]))
+    if (parseInt(a[3]) < parseInt(b[3]))
       return 1;
     return -1;
   });
@@ -117,7 +117,7 @@ onMounted(() => {
     if (message.type === 'propsChange') {
       const { newProps } = message.payload;
       loadSpreadSheet(newProps.apiKey);
-      setInterval(() => loadSpreadSheet(newProps.apiKey), newProps.refreshRate * 1000)
+      setInterval(() => loadSpreadSheet("AIzaSyCasDc28gfYMH1La9vxH3IMfHYgM_Yq8Rk"), newProps.refreshRate * 1000)
       // mettre à jour l'affichage du plugin en utilisant newProps
     }
   });
